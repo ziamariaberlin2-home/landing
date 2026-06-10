8cb53a48d88da14bdf95950ddf28c7c9d2e5de99
ziamariaberlin2-home <ziamariaberlin2@gmail.com>
2026-06-10 19:51:26 +0000

    updated template in forms

--- assets/js/forms.js
+++ assets/js/forms.js
@@ -9,8 +9,8 @@
 // 5. Replace the three placeholder strings below with your real values
 
 const EMAILJS_PUBLIC_KEY = 'fJjxFb00WcpGc9lDX';
-const EMAILJS_RESERVATION_TEMPLATE = 'template_aul6fwl';
-const EMAILJS_HOST_TEMPLATE = 'template_q4vwcwm';
+const EMAILJS_RESERVATION_TEMPLATE = 'template_q4vwcwm';
+const EMAILJS_HOST_TEMPLATE = 'template_aul6fwl';
 const EMAILJS_SERVICE_ID = 'service_kbchpcs';
 
 
